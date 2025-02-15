d7cdfb726a8967189240a1e098675a15847197c2
Fred Alisson da Silva Sousa <fredalissonsousa@gmail.com>
2025-02-14 22:45:29 -0300

    refact: using API utils and refact code

--- src/bronze/alpha_vantage_api.py
+++ src/bronze/alpha_vantage_api.py
@@ -1,31 +1,16 @@
-import os
-import requests
 from dotenv import load_dotenv
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 from utils.logging_setup import LoggingSetup
+from utils.api_utils import APIUtils
+import pandas as pd
 
 load_dotenv()
 log_br = LoggingSetup.get_bronze_logger()
 
-class AlphaVantageAPI:
+class AlphaVantageAPIFetcher(APIUtils):
     BASE_URL: str = "https://www.alphavantage.co/query"
     DEFAULT_TIMEOUT: int = 10
-    _API_KEY: Optional[str] = None
     
-    @classmethod
-    def _get_api_key(cls) -> str:
-        """
-        Retrieve and cache the API key from environment variables.
-        Raises:
-            ValueError: If the API key is missing.
-        """
-        if cls._API_KEY is None:
-            cls._API_KEY = os.getenv("API_ALPHA_VANTAGE_KEY")
-            if not cls._API_KEY:
-                log_br.error("API key is missing. Please check your environment variables.")
-                raise ValueError("API key missing.")
-        return cls._API_KEY
-
     @classmethod
     def _setup_params(cls, symbol: str) -> dict:
         """
@@ -34,7 +19,7 @@ class AlphaVantageAPI:
         return {
             "function": "HISTORICAL_OPTIONS",
             "symbol": symbol,
-            "apikey": cls._get_api_key(),
+            "apikey": cls._get_api_key(name_key="API_ALPHA_VANTAGE_KEY"),
             "outputsize": "full"
         }
 
@@ -47,22 +32,21 @@ class AlphaVantageAPI:
             A dictionary with API data if successful; otherwise, None.
         """
         params = cls._setup_params(symbol)
-
-        try:
-            log_br.info(f"Fetching data for symbol: {symbol}")
-            response = requests.get(cls.BASE_URL, params=params, timeout=cls.DEFAULT_TIMEOUT)
-            response.raise_for_status()
-
-            data = response.json()
-            if "Error Message" in data:
-                log_br.error(f"Error fetching data for {symbol}: {data['Error Message']}")
-                return None
-            elif "Note" in data:
-                log_br.warning(f"Rate limit hit: {data['Note']}")
-                return None
-
-            return data
-        except (requests.exceptions.RequestException, ValueError) as e:
-            log_br.error(f"Request failed: {e}")
+        data = cls._fetch_data(params=params)
+        if "Error Message" in data:
+            log_br.error(f"Error fetching data for {symbol}: {data['Error Message']}")
             return None
-
+        elif "Note" in data:
+            log_br.warning(f"Rate limit hit: {data['Note']}")
+            return None
+        return data
+        
+    @classmethod
+    def fetch_batch_data(cls, symbols: List[str]):
+        results: dict = {}
+        for symbol in symbols:
+            log_br.info(f"Fetching data for symbol: {symbol}")
+            data = cls.fetch_data(symbol)
+            results[symbol] = data
+            
+        return results
